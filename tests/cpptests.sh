39963f779326ad2d5b6e4b2fbda9436170bb450c
Ildar Gafurov <dev@binblob.com>
2022-08-26 14:38:01 +0300

    tests/cpptests.sh: fix for a jenkinsworker

--- tests/cpptests.sh
+++ tests/cpptests.sh
@@ -9,9 +9,10 @@ TEST_FILES="${ROOT_DIR}/tests/*_test.cpp"
 CGC_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_cgc/cgc.cpp"
 CMCQUEUE_CPP_FILES="${ROOT_DIR}//wasp_c_extensions/_cmcqueue/cmcqueue.cpp"
 
-g++ -I"${ROOT_DIR}" -I"${ROOT_DIR}/wasp_c_extensions" -lcppunit -o "${CPP_EXEC}" \
+g++ -I"${ROOT_DIR}" -I"${ROOT_DIR}/wasp_c_extensions" \ 
     ${TEST_FILES} \
     ${CGC_CPP_FILES} \
-    ${CMCQUEUE_CPP_FILES}
+    ${CMCQUEUE_CPP_FILES} \
+    -lcppunit -o "${CPP_EXEC}"  # -lcppunit should be at the end because of some g++ issue on jenkins worker
 
 ${CPP_EXEC}
