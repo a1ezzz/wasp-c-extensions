766bba3e2f48b8a8ab0d8e7dd88b3b5c755026d8
Ildar Gafurov <dev@binblob.com>
2022-08-26 20:05:34 +0300

    Jenkinsfile: coveralls test (try #11)

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 
 ![build/test status](https://jenkins.home.a1ezzz.ru/buildStatus/icon?job=wasp-c-extensions%2Fmain&subject=build/tests)
+[![Coverage Status](https://coveralls.io/repos/github/a1ezzz/wasp-c-extensions/badge.svg?branch=main)](https://coveralls.io/github/a1ezzz/wasp-c-extensions?branch=main)
 [![Python Version](https://img.shields.io/pypi/pyversions/wasp-c-extensions.svg)](https://pypi.python.org/pypi/wasp-c-extensions)
 
 A library with bunch of CPython extensions
-
-
-
--- tests/cpptests.sh
+++ tests/cpptests.sh
@@ -9,11 +9,17 @@ CPP_EXEC="${ROOT_DIR}/${1:-cpp-tests-exec}"
 TEST_FILES="$(dirname $0)/*_test.cpp"
 CGC_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_cgc/cgc.cpp"
 CMCQUEUE_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_cmcqueue/cmcqueue.cpp"
+EVLOOP_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_evloop/evloop.cpp"
+PQUEUE_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_pqueue/pqueue.cpp"
+THREADS_CPP_FILES="${ROOT_DIR}/wasp_c_extensions/_threads/event.cpp"
 
 g++ -fprofile-filter-files='.*\.cpp' --coverage -I"${ROOT_DIR}" -I"${ROOT_DIR}/wasp_c_extensions" \
     ${TEST_FILES} \
     ${CGC_CPP_FILES} \
     ${CMCQUEUE_CPP_FILES} \
+    ${EVLOOP_CPP_FILES}" \
+    ${PQUEUE_CPP_FILES}" \
+    ${THREADS_CPP_FILES}" \
     -lcppunit -o "${CPP_EXEC}"  # -lcppunit should be at the end because of some g++ issue on jenkins worker
 
 ${CPP_EXEC}
