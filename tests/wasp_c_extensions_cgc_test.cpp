a0b5eee30ce9f4f5e9f001b209ea11bc200ee193
Ildar Gafurov <dev@binblob.com>
2022-08-26 14:23:44 +0300

    tests: cpptests updated

--- tests/wasp_c_extensions_cgc_test.cpp
+++ tests/wasp_c_extensions_cgc_test.cpp
@@ -1,46 +1,24 @@
 
 #include <cppunit/TestCase.h>
 #include <cppunit/extensions/HelperMacros.h>
-#include <cppunit/BriefTestProgressListener.h>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/TestResultCollector.h>
-#include <cppunit/TestRunner.h>
+
+#include "wasp_c_extensions/_cgc/cgc.hpp"
 
 
-class TestExample:
+class TestWaspCGCTest:
     public CppUnit::TestFixture
 {
 
-    CPPUNIT_TEST_SUITE(TestExample);
+    CPPUNIT_TEST_SUITE(TestWaspCGCTest);
     CPPUNIT_TEST(test);
     CPPUNIT_TEST_SUITE_END();
 
-    public:
-        void test(){
-            CPPUNIT_ASSERT(1 == 1);
-        }
+    void test(){
+        wasp::cgc::ConcurrentGarbageCollector* cgc = new wasp::cgc::ConcurrentGarbageCollector();
+        CPPUNIT_ASSERT(1 == 1);
+        
+        delete cgc;
+    }
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(TestExample);
-
-int main(int argc, char* argv[])
-{
-    CppUnit::TestResult controller;
-    CppUnit::TestResultCollector result;
-    CppUnit::BriefTestProgressListener progress;
-    CppUnit::TestRunner runner;
-    
-    controller.addListener(&result);
-    controller.addListener(&progress);
-    
-    runner.addTest(CppUnit::TestFactoryRegistry::getRegistry().makeTest());
-    runner.run(controller);
-
-    CppUnit::CompilerOutputter(&result, CppUnit::stdCOut()).write(); 
-    
-    return result.wasSuccessful() ? 0 : 1;
-}
-
-
+CPPUNIT_TEST_SUITE_REGISTRATION( TestWaspCGCTest);
